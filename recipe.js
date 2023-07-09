cdbe13133c84bb1f323ef45d9e823793d331df7e
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-09 17:01:33 -0400

    Change background color from light to dark

--- recipe.js
+++ recipe.js
@@ -4,3 +4,11 @@ function welcome() {
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('button').onclick = welcome;
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#dark').onclick = () => {
+        document.querySelector('body').style.backgroundColor = 'hsl(230, 20%, 20%)';
+        document.querySelector('section').style.backgroundColor = 'hsl(230, 20%, 50%';
+        document.querySelector('h1').style.color = 'hsl(350, 20%, 80%';
+    }
+    })
