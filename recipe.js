f271c43b25490bc24aab939b91196400ed17d363
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-11 10:58:00 -0400

    Add gradient-background

--- recipe.js
+++ recipe.js
@@ -8,7 +8,12 @@ document.addEventListener("DOMContentLoaded", function(){
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#dark').onclick = () => {
         document.querySelector('body').style.backgroundColor = 'hsl(230, 20%, 20%)';
-        document.querySelector('section').style.backgroundColor = 'hsl(230, 20%, 50%';
-        document.querySelector('h1').style.color = 'hsl(350, 20%, 80%';
+        document.querySelector('section').style.backgroundColor = 'hsl(230, 20%, 50%)';
+        document.querySelector('h1').style.color = 'hsl(350, 20%, 80%)';
+        const nodeLinks = document.querySelectorAll('.list');
+        for (let i = 0; i < nodeLinks.length; i++) {
+            nodeLinks[i].style.color = 'hsl(350, 20%, 80%)';
+        }
+    
     }
-    })
+})
